5bfaca4491850f5b25d6b01b5cc87cb2f45ec367
raeed1993 <raeed1993@gmail.com>
2022-07-23 12:17:39 +0300

    fix services

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -23,37 +23,61 @@
         </div>
     </section>
 
-    <section class="services-pages d-none d-sm-block section-padding">
-        <div class="container">
-            <div class="row">
-                @foreach($services as $service)
+{{--    <section class="services-pages d-none d-sm-block section-padding">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                @foreach($services as $service)--}}
 
-                    <div class="col-sm-6 mb-3 col-md-6">
-                        <a href="{{route('taxonomy.show',$service->translate('en')->slug)}}"
-                           class="text-black text-decoration-none">
-                            <div class="content-services">
-                                {{--                            <i class="uil uil-video"></i>--}}
-                                <img src="{{$service->primary_image}}" alt="{{$service->translate('en')->title}}">
-                                <h3 class="text-lg">{{$service->title}}</h3>
+{{--                    <div class="col-sm-6 mb-3 col-md-6">--}}
+{{--                        <a href="{{route('taxonomy.show',$service->translate('en')->slug)}}"--}}
+{{--                           class="text-black text-decoration-none">--}}
+{{--                            <div class="content-services">--}}
+{{--                                --}}{{--                            <i class="uil uil-video"></i>--}}
+{{--                                <img src="{{$service->primary_image}}" alt="{{$service->translate('en')->title}}">--}}
+{{--                                <h3 class="text-lg">{{$service->title}}</h3>--}}
+{{--                            </div>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+
+
+{{--                @endforeach--}}
+
+
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="text-center">--}}
+{{--            <a href="{{route('contact.page')}}" class="btn btn-primary btn-view">--}}
+{{--                <b>--}}
+{{--                    تواصل معنا--}}
+{{--                </b>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--    </section>--}}
+    <section class="business-services  d-none d-sm-block section-padding" id="business">
+        <div class="container">
+
+            <div class="row">
+                @foreach($services as $project)
+                    <div class="col-sm-6 mb-3">
+                        <a href="{{route('taxonomy.show',$project->translate('en')->slug)}}"
+                           class="text-decoration-none">
+                            <div class="content-business">
+                                <div class="content-business_img">
+                                    <img src="{{$project->primary_image}}" alt="{{$project->translate('en')->title}}"/>
+                                    <h2>{{$project->title}}</h2>
+                                </div>
+
                             </div>
                         </a>
                     </div>
-
-
                 @endforeach
-
-
+                <div class="swiper-pagination"></div>
             </div>
-        </div>
-        <div class="text-center">
-            <a href="{{route('contact.page')}}" class="btn btn-primary btn-view">
-                <b>
-                    تواصل معنا
-                </b>
-            </a>
-        </div>
 
+        </div>
     </section>
+
     <section class="services-pages services-pages-mobile-section d-block d-sm-none  section-padding" id="services">
         <div class="container">
 
