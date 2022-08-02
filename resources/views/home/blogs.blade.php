abd8cc55b1d05850f319d67c97e40acb416cb5a6
raeed1993 <raeed1993@gmail.com>
2022-08-02 10:03:11 +0300

    fix blogs add slider in mobile

--- resources/views/home/blogs.blade.php
+++ resources/views/home/blogs.blade.php
@@ -1,4 +1,4 @@
-<section class="article section-padding" id="article">
+<section class="article  d-none d-sm-block   section-padding" id="article">
     <div class="container">
         <div class="title-text-light">
             <h2>
@@ -31,5 +31,34 @@
                 </b>
             </a>
         </div>
+    </div>
+</section>
+<section class="business business-mobile-section d-block d-sm-none section-padding" id="business">
+    <div class="container">
+
+        <div class="swiper business-mobile">
+            <div class="swiper-wrapper">
+                @foreach($blogs as $blog)
+                    <section class="swiper-slide">
+                        <a href="{{route('taxonomy.show',$blog->translate('en')->slug)}}"
+                           class="text-decoration-none">
+                            <div class="content-business">
+                                <div class="content-business_img">
+                                    <img src="{{$blog->primary_image}}" alt="{{$blog->translate('en')->title}}"/>
+                                </div>
+                                <h2>{{$blog->title}}</h2>
+                                <section>
+                                    {!!  implode(' ', array_slice(explode(' ', $blog->content), 0, 15))!!}
+                                </section>
+                                @include('partials.continue-read')
+
+                            </div>
+                        </a>
+                    </section>
+                @endforeach
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
     </div>
 </section>
